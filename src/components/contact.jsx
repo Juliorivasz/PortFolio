d904a6f5323c4fbff8bf89665ec0ed9aa170ecba
Julio Rivas <julioandresrivas@gmail.com>
2022-10-31 18:52:00 -0300

    add email

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,13 +14,13 @@ function Contact(){
                 <p>juliorivasdev@gmail.com</p>
                 <p>(+54) 261-575-2890</p>
             </div>
-            <form className="form__contact" action="https://formsubmit.co/70add80e68d1397fe0f4ecc6de9284 21" method="POST" >
+            <form className="form__contact" action="https://formsubmit.co/juliorivasdev@gmail.com" method="POST" >
                 <input type="text" className="input__form" name="name" id="name" placeholder="Name" required/>
                 <input type="email" className="input__form" name="email" id="email" placeholder="Email" required/>
                 <input type="text" className="input__form" name="subject" id="subject" placeholder="Subject" required/>
                 <textarea type="textarea" className="input__form" name="message" id="message" placeholder="Message" required />
                 <input type="submit" className="input__form" name="submit" id="submit" required/>
-                <input type="hidden" name="_next" value="https://juliorivasz.github.io/PortFolio/#contact"/>
+                <input type="hidden" name="_next" value="https://juliorivasz.github.io/PortFolio/"/>
                 <input type="hidden" name="_captcha" value="false"/>
             </form>
         </section>
