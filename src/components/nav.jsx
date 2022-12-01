1de1d57e57fb3bdd34b5ed659d3f628b014049e7
Julio Rivas <julioandresrivas@gmail.com>
2022-12-01 12:48:47 -0300

    menu animated

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,14 +3,33 @@ import '../css/nav.css';
 import menu from '../img/svg/bx-menu.svg';
 import eks from '../img/eks.png';
 import profile from '../img/julio.png';
+import { useState } from "react";
 
 function Nav({ urlLink, diss, dissemble,theme, setDissemble }) {
+
+  const [changes, setChanges] = useState("not-active")
+
+  const changesMenu = ()=>{
+    if(changes === "not-active" && diss==="dissemble"){
+      setChanges("active")
+      dissemble()
+    }else if(changes==="active" && diss===""){
+      setChanges("not-active")
+      dissemble()
+    }
+  }
+
+  const changesImg = ()=>{
+    setChanges("not-active");
+    setDissemble('dissemble')
+    
+  }
     return (
         <nav className={`mobile ${theme}`}>
           <div className="container__nav">
             <div className="container__img__perfil">
               <a className="link__perfil" href={window.location.pathname+'#main'}>
-                <img src={profile} className={`name__portfolio`} onClick={()=>setDissemble('dissemble')}/>
+                <img src={profile} className={`name__portfolio`} onClick={changesImg}/>
                 <p className="navegation__bar desktop">Home</p>
               </a>
             </div>
@@ -21,10 +40,15 @@ function Nav({ urlLink, diss, dissemble,theme, setDissemble }) {
               <a className="desktop" href={urlLink+'#contact'}><p className="navegation__bar">Contact me</p></a>
               <a href="./CV Spanish julio.pdf" download><p className="navegation__bar cv">Download CV</p></a>
             </div>
-            <img className={`img__menu ${diss === 'dissemble'? '' : 'dissemble'}`} src={menu} alt="menu" title="menu"
+            <div className={`btn__menu ${changes}`} onClick={changesMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            {/* <img className={`img__menu ${diss === 'dissemble'? '' : 'dissemble'}`} src={menu} alt="menu" title="menu"
              onClick={dissemble}/>
             <img className={`img__menu x ${diss === 'dissemble'? 'dissemble' : ''}`} src={eks} alt="menu" title="menu"
-             onClick={dissemble}/>
+             onClick={dissemble}/> */}
           </div>
         </nav>
     );
