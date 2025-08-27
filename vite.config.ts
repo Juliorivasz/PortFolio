de418199764b14e7c9e26f1ab923a54fef804566
julio Rivas <juliorivasdev@gmail.com>
2025-08-27 13:02:57 -0300

    limpie la config de /portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/PortFolio/",
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
